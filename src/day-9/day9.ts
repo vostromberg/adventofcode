2d1f9a5e2876288ba58894ce9b09a2b0e8f79e24
viktor.ostromberg <viktor.ostromberg@headlight.se>
2020-01-13 20:53:57 +0100

    Day9 part2

--- src/day-9/day9.ts
+++ src/day-9/day9.ts
@@ -4,13 +4,11 @@ import { day9input } from "./day9-input";
 
 
 export const day9part1 = async () => {
-    console.log(day9input);
-    const programResult = runProgram(day9input,1);
-    console.log(programResult);
+    const programResult = runProgram(day9input, 1);
     return programResult.output.join(",");
 }
 
 export const day9part2 = async () => {
-
-    return "Not finished yet";
+    const programResult = runProgram(day9input, 2);
+    return programResult.output.join(",");
 }
